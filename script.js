87c62b312ed0220a1f3e1e8bc4081adb06512ba8
dnlespinosa <89487129+dnlespinosa@users.noreply.github.com>
2022-01-21 10:43:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -49,20 +49,59 @@
 //     }
 // };
 
-function Triangle(a, b) {
-    this.a = a;
-    this.b = b;
+// function Triangle(a, b) {
+//     this.a = a;
+//     this.b = b;
+// }
+
+// const tri1 = new Triangle(3,4);
+// const tri2 = new Triangle(9,12);
+
+// Triangle.prototype.getArea = function() {
+//     return (this.a*this.b)/2
+// };
+// Triangle.prototype.getHypotenuse = function() {
+//     return Math.sqrt(this.a**2 + this.b**2);
+// };
+
+class Triangle {
+    constructor(a,b,c) {
+        for (let side of [a,b,c]){
+            if(!Number.isFinite(side) || side <= 0) {
+                throw new Error('Sides must be positive numbers!!!!');
+            }
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    greet(){
+        console.log('hello from the other side!!!');
+    }
+    display(){
+        console.log(`Triangle with the sides of ${this.a} and ${this.b}`);
+    }
+    getArea() {
+        const {a,b,c} = this;
+        const s = (a+b+c)/2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    isBig() {
+        return this.getArea() >50
+    }
 }
 
-const tri1 = new Triangle(3,4);
-const tri2 = new Triangle(9,12);
+// const firstTri = new Triangle();
+// firstTri.a = 3;
+// firstTri.b = 4;
+// const secondTri = new Triangle();
+// secondTri.a = 9;
+// secondTri.b = 12;
 
-Triangle.prototype.getArea = function() {
-    return (this.a*this.b)/2
-};
-Triangle.prototype.getHypotenuse = function() {
-    return Math.sqrt(this.a**2 + this.b**2);
-};
+
+
+
+
 
 
 
